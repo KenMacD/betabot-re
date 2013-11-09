3f63e8ae34a5916e80cb84b405895f0a5237c86a
Kenny MacDermid <kenny.macdermid@gmail.com>
2013-11-08 20:12:51 -0400

    Add v1.6 support, and refactor to classes.
    The new version adds an extra obfuscation step to the host name,
    and an xor to the encrypted data.

--- bb_dump_config.py
+++ bb_dump_config.py
@@ -19,11 +19,221 @@ from Crypto.Cipher import ARC4
 
 KEY_SEARCH_RANGE = 0x200
 
-HEADER_v1   = "".join([chr(c) for c in [0xce, 0x2a, 0xaa, 0x0f, 0x92, 0xc1]])
-HEADER_v1p5 = "".join([chr(c) for c in [0x46, 0x0d, 0xaa, 0x0f, 0x92, 0xc1]])
+class Config(object):
+    """Base class for configurations, subclasses my have the properties:
+       To find binary config:
+         header         = header of a valid config
+         config_size    = size of the full config block with all sections
+         needle         = easy to find value close to the config
+         needle_offset  = offset from needle to start searching
+         config_version = version string for config
+       To unpack config:
+         owner_offset, s1_offset, s2_offset
+         config_section_start, config_section_size
+         host_offset, path_offset, port_offset, ssl_offset
+         attempts_offset, key1_offset, key2_offset
+    """
+    num_servers = 16
 
-SIZE_v1   = 0x2ace
-SIZE_v1p5 = 0x0d46
+    @classmethod
+    def encrypt(cls, data, key):
+        """Encrypt and decrypt are the same for RC4."""
+        rc4 = ARC4.new(key)
+        return rc4.encrypt(data)
+
+    @classmethod
+    def make_key(cls, data, offset):
+        """Return every second value for 16 bytes after"""
+        return data[offset:offset+0x20:2]
+
+    @classmethod
+    def needle_locs(cls, data):
+        offset = data.find(cls.needle)
+        while offset != -1:
+            yield offset
+            offset = data.find(cls.needle, offset + 1)
+
+    @classmethod
+    def keys(cls, data, location):
+        for key_loc in range(location, location + KEY_SEARCH_RANGE):
+            yield cls.make_key(data, key_loc)
+
+    @classmethod
+    def offset_search(cls, data):
+        if cls.header in data:
+            offset = data.find(cls.header)
+            return data[offset:offset+cls.config_size]
+
+        for needle_loc in cls.needle_locs(data):
+            loc = needle_loc + cls.needle_offset
+
+            # Not enough room for a config to exist.
+            # Misses a little bit where loc < 0 but loc + search range > 0, but
+            # this shouldn't be a problem
+            if (loc < 0) or (loc + cls.config_size > len(data)):
+                continue
+
+            for key in cls.keys(data, loc):
+                needed = cls.encrypt(cls.header, key)
+                config_loc = data.find(needed)
+                if config_loc != -1:
+                    econfig = data[config_loc:config_loc+cls.config_size]
+                    config = cls.encrypt(econfig, key)
+                    return cls(config)
+
+    def __init__(self, config):
+        (self.owner,) = unpack_string(config, self.owner_offset)
+        (self.s1,)    = unpack_unicode(config, self.s1_offset)
+        (self.s2,)    = unpack_unicode(config, self.s2_offset)
+
+        self.servers = []
+        for i in range(self.num_servers):
+            offset = self.config_section_start + (i * self.config_section_size)
+            (clen,) = struct.unpack_from("<H", config, offset)
+
+            if clen != self.config_section_size:
+                continue
+
+            section = {}
+            (section["host"],)     = unpack_string(config, offset + self.host_offset)
+            (section["path"],)     = unpack_string(config, offset + self.path_offset)
+            (section["port"],)     = struct.unpack_from("<H", config, offset + self.port_offset)
+            (section["ssl"],)      = struct.unpack_from("?", config, offset + self.ssl_offset)
+            (section["attempts"],) = struct.unpack_from("<H", config, offset + self.attempts_offset)
+            (section["key1"],)     = struct.unpack_from("8s", config, offset + self.key1_offset)
+            (section["key2"],)     = struct.unpack_from("8s", config, offset + self.key2_offset)
+
+            self.servers.append(section)
+
+    def __str__(self):
+        lines = []
+        lines.append("BetaBot Config (version %s):"%(self.config_version,))
+        lines.append("  Owner: %s"%(self.owner,))
+        lines.append("  String1: %s"%(self.s1,))
+        lines.append("  String2: %s"%(self.s2,))
+        for (i, c) in enumerate(self.servers):
+            lines.append("  Config %d:"%(i,))
+            SSL = c["ssl"]
+            port = c["port"]
+
+            if SSL:
+                url = "https://"
+            else:
+                url = "http://"
+
+            url = url + c["host"]
+
+            if not ((port == 80 and not SSL) or (port == 443 and SSL)):
+                url = url + ":" + str(port)
+
+            url = url + str(c["path"])
+
+    #        lines.append("Match: %s,%s,%d,%s,%s,%s,%s"%(config["Owner"], config["_ver"],
+    #                                i+1, url, str(c["Attempts"]), str(c["Key1"]),
+    #                                str(c["Key2"]))
+            lines.append("    URL: " + url)
+            lines.append("    Attempts: " + str(c["attempts"]))
+            lines.append("    Key1: " + str(c["key1"]).encode("hex").upper())
+            lines.append("    Key2: " + str(c["key2"]).encode("hex").upper())
+
+        return "\n".join(lines)
+
+
+class Config_v1(Config):
+    config_version = "1.0.2.5"
+    header         = "".join([chr(c) for c in [0xce, 0x2a, 0xaa, 0x0f, 0x92, 0xc1]])
+    config_size    = 0x2ace
+    needle         = "crypt32.dll"
+    needle_offset  = -0xd00 # -0xc52
+
+    owner_offset = 6
+    s1_offset    = 0x14e
+    s2_offset    = 0x24e
+    # A backup url exists, but it's always the same, so uninteresting
+    #(backup_url,) = unpack_string(data, 0x44)
+    #config_struct["Backup_URL"] = backup_url
+
+    config_section_start = 0x2ce
+    config_section_size  = 640
+
+    host_offset     = 0x66
+    path_offset     = 0x166
+    port_offset     = 0x14
+    ssl_offset      = 0x1e
+    attempts_offset = 0x12
+    key1_offset     = 0x26e
+    key2_offset     = 0x277
+
+class Config_v1p5(Config):
+    config_version = "1.5"
+    header         = "".join([chr(c) for c in [0x46, 0x0d, 0xaa, 0x0f, 0x92, 0xc1]])
+    config_size    = 0x0d46
+    needle         = "crypt32.dll"
+    needle_offset  = -0xd80 # -0xd0e
+
+    owner_offset = 6
+    s1_offset    = 0x46
+    s2_offset    = 0xc6
+
+    config_section_start = 0x146
+    config_section_size  = 192
+
+    host_offset     = 0x26
+    path_offset     = 0x66
+    port_offset     = 0x14
+    ssl_offset      = 0x1e
+    attempts_offset = 0x12
+    key1_offset     = 0xae
+    key2_offset     = 0xb7
+
+class Config_v1p6(Config_v1p5):
+    config_version = "1.6"
+    header         = "".join([chr(c) for c in [0x46, 0x0d, 0xaa, 0x0f, 0x92, 0xc1]])
+    config_size    = 0x0d46
+    needle         = "crypt32.dll"
+    needle_offset  = -0xd80 # -0xd0e
+
+    @classmethod
+    def make_key(cls, data, offset):
+        key = super(Config_v1p6, cls).make_key(data, offset)
+        return "".join([chr(ord(x) ^ 0x2e) for x in key])
+
+    @classmethod
+    def encrypt(cls, data, key):
+        decrypt = super(Config_v1p6, cls).encrypt(data, key)
+        xdecrypt = ""
+        for i in range(len(decrypt)):
+            xdecrypt += chr(ord(decrypt[i]) ^ ord(key[i%4]))
+        return xdecrypt
+
+    def __init__(self, config):
+        super(Config_v1p6, self).__init__(config)
+        for server in self.servers:
+            server['host'] = self._deobfuscate_host(server['host'])
+
+    def _deobfuscate_host(self, host):
+        slen = len(host)
+
+        array = [ord(x) for x in host]
+
+        if slen < 8 or slen > 0x40:
+            return host
+
+        val_0 = array[0]
+
+        loc1 = (val_0 + 2 * slen) % (slen - 2) + 1
+        loc2 = (val_0 + 8 * (slen + 1)) % (slen - 3) + 2
+
+        xor1 = 655  * val_0 % 3 + 24
+        xor2 = 1424 * val_0 % 6 + 23
+
+        if xor1 != array[loc1]:
+            array[loc1] ^= xor1
+
+        if xor2 != array[loc2]:
+            array[loc2] ^= xor2
+
+        return "".join([chr(x) for x in array])
 
 def unpack_string(buf, offset):
     null_len = buf[offset:].find('\0')
@@ -34,191 +244,6 @@ def unpack_unicode(buf, offset):
     bufcopy = buf[offset::2]
     return unpack_string(bufcopy, 0)
 
-def get_decyrpted_config(data, header, search_data, offset, config_size):
-    if header in data:
-        print " [*] Decrypted version may already exist"
-
-    crypt_offset = data.find(search_data)
-    while crypt_offset != -1:
-        key_start = crypt_offset + offset
-        
-        crypt_offset = data.find(search_data, crypt_offset + 1)
-        if (key_start < 0) or (key_start + config_size > len(data)):
-                continue
-
-        for koffset in range(key_start, key_start + KEY_SEARCH_RANGE):
-            key = data[koffset:koffset+0x20:2]
-            rc4 = ARC4.new(key)
-
-            needed = rc4.encrypt(header)
-            conf_location = data.find(needed)
-            if conf_location != -1:
-                if data.find(needed, conf_location + 1) != -1:
-                    print " [*] Warning, multiple locations found"
-                crypted_config = data[conf_location:conf_location+config_size]
-
-                rc4 = ARC4.new(key)
-                config = rc4.decrypt(crypted_config)
-                return config
-    return None
-
-def offset_search_v1(data):
-    header = HEADER_v1
-    offset = -0xd00 # -0xc52 in my sample
-
-    return get_decyrpted_config(data, header, "crypt32.dll", offset, SIZE_v1)
-
-def offset_search_v1p5(data):
-    header = HEADER_v1p5
-    offset = -0xf00 # -0xd0e in my sample
-
-    return get_decyrpted_config(data, header, "crypt32.dll", offset, SIZE_v1p5)
-
-def decode_config_v1(data):
-    config_struct = {"Config": []}
-    if len(data) != SIZE_v1:
-        return ""
-
-    # A backup url exists, but it's always the same, so uninteresting
-    #(backup_url,) = unpack_string(data, 0x44)
-    #config_struct["Backup_URL"] = backup_url
-
-    config_struct["_ver"] = "1.0.2.5"
-
-    (owner,) = unpack_string(data, + 6)
-    (s1,) = unpack_unicode(data, 0x14e)
-    (s2,) = unpack_unicode(data, 0x24e)
-    config_struct["Owner"] = owner
-    config_struct["String1"] = s1
-    config_struct["String2"] = s2
-
-    for i in range(16): # There are up to 16 configs
-        offset = 0x2ce + (i * 640)
-        (clen,) = struct.unpack_from("<H", data, offset)
-
-        if clen == 640:
-            config = {}
-            (host,) = unpack_string(data, offset + 0x066)
-            (path,) = unpack_string(data, offset + 0x166)
-            (port,) = struct.unpack_from("<H", data, offset + 0x14)
-            (ssl, ) = struct.unpack_from("?", data, offset + 0x1e)
-            (attempts,) = struct.unpack_from("<H", data, offset + 0x12)
-            (key1,) = struct.unpack_from("8s", data, offset + 0x26e)
-            (key2,) = struct. unpack_from("8s", data, offset + 0x277)
-
-            config["Host"] = host
-            config["Path"] = path
-            config["Port"] = port
-            config["SSL"] = ssl
-            config["Attempts"] = attempts
-            config["Key1"] = key1.encode("hex").upper()
-            config["Key2"] = key2.encode("hex").upper()
-            
-            config_struct["Config"].append(config)
-
-    return config_struct
-
-def decode_config_v1p5(data):
-    config_struct = {"Config": []}
-    if len(data) != SIZE_v1p5:
-        return ""
-
-    config_struct["_ver"] = "1.5"
-    
-    (owner,) = unpack_string(data, + 6)
-    (s1,) = unpack_unicode(data, 0x46)
-    (s2,) = unpack_unicode(data, 0xc6)
-    config_struct["Owner"] = owner
-    config_struct["String1"] = s1
-    config_struct["String2"] = s2
-
-    for i in range(16): # There are up to 16 configs
-        offset = 0x146 + (i * 192)
-        (clen,) = struct.unpack_from("<H", data, offset)
-
-        if clen == 192:
-            config = {}
-            (host,) = unpack_string(data, offset + 0x26)
-            (path,) = unpack_string(data, offset + 0x66)
-            (port,) = struct.unpack_from("<H", data, offset + 0x14)
-            (ssl, ) = struct.unpack_from("?", data, offset + 0x1e)
-            (attempts,) = struct.unpack_from("<H", data, offset + 0x12)
-            (key1,) = struct.unpack_from("8s", data, offset + 0xae)
-            (key2,) = struct. unpack_from("8s", data, offset + 0xb7)
-
-            config["Host"] = host
-            config["Path"] = path
-            config["Port"] = port
-            config["SSL"] = ssl
-            config["Attempts"] = attempts
-            config["Key1"] = key1.encode("hex").upper()
-            config["Key2"] = key2.encode("hex").upper()
-            
-            config_struct["Config"].append(config)
-
-    return config_struct
-
-def print_config(config):
-    print "BetaBot Config (version %s):"%(config["_ver"],)
-    print "  Owner: %s"%(config["Owner"],)
-    print "  String1: %s"%(config["String1"],)
-    print "  String2: %s"%(config["String2"],)
-    for (i, c) in enumerate(config["Config"]):
-        print "  Config %d:"%(i,)
-        SSL = c["SSL"]
-        port = c["Port"]
-
-        if SSL:
-            url = "https://"
-        else:
-            url = "http://"
-
-        url = url + c["Host"]
-
-        if not ((port == 80 and not SSL) or (port == 443 and SSL)):
-            url = url + ":" + str(port)
-
-        url = url + str(c["Path"])
-
-#        print "Match: %s,%s,%d,%s,%s,%s,%s"%(config["Owner"], config["_ver"],
-#                                i+1, url, str(c["Attempts"]), str(c["Key1"]),
-#                                str(c["Key2"]))
-        print "    URL: " + url
-        print "    Attempts: " + str(c["Attempts"])
-        print "    Key1: " + str(c["Key1"])
-        print "    Key2: " + str(c["Key2"])
-
-def build_index(data):
-    indexed = {}
-
-    for i in range(len(data) - 6):
-        key = data[i:i+6]
-        if key in indexed:
-            indexed[key].append(i)
-        else:
-            indexed[key] = [i]
-
-    return indexed
-
-def bf_search_config(data, data_index, header, size):
-
-    for i in range(len(data) - 0x20):
-        key = data[i:i+0x20:2]
-
-        rc4 = ARC4.new(key)
-        needed = rc4.decrypt(header)
-
-        if needed in data_index:
-            offsets = data_index[needed]
-            if len(offsets) > 1:
-                print "    [*] Multiple possible config locations found: " + str(offsets)
-            offset = offsets[0]
-
-            rc4 = ARC4.new(key)
-            return rc4.decrypt(data[offset:offset+size])
-    return None
-
-
 def main():
     if len(sys.argv) != 2:
         print "Usage: {0} <dumpfile>".format(sys.argv[0])
@@ -228,42 +253,27 @@ def main():
         data = sample.read()
 
     print " [*] Offset searching for version 1 config"
-    config = offset_search_v1(data)
+    config = Config_v1.offset_search(data)
     if config:
         print "   [*] Found version 1 config"
-        print_config(decode_config_v1(config))
+        print str(config)
         return
 
     print " [*] Offset searching for version 1.5 config"
-    config = offset_search_v1p5(data)
+    config = Config_v1p5.offset_search(data)
     if config:
         print "   [*] Found version 1.5 config"
-        print_config(decode_config_v1p5(config))
+        print str(config)
         return
 
-    print " [*] Building index... ",
-    index = build_index(data)
-    print "Done"
-
-    print " [*] Bruteforce searching for version 1 config"
-    config = bf_search_config(data, index, HEADER_v1, SIZE_v1)
+    print " [*] Offset searching for version 1.6 config"
+    config = Config_v1p6.offset_search(data)
     if config:
-        print "    [*] Found version 1 config"
-        print_config(decode_config_v1(config))
+        print "   [*] Found version 1.6 config"
+        print str(config)
         return
 
-    print " [*] Bruteforce searching for version 1.5 config"
-    config = bf_search_config(data, index, HEADER_v1p5, SIZE_v1p5)
-    if config:
-        print "    [*] Found version 1.5 config"
-        print_config(decode_config_v1p5(config))
-        return
-
-    if config:
-        sys.exit(0)
-    else:
-        print "no config"
-        sys.exit(1)
+    print " [*] No configs found"
 
 if __name__ == "__main__":
     main()
